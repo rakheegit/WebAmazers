59aaf6288f71c802c30ba2e475b20722a398b3eb
Rakhee Singh <rakheesmailbag@gmail.com>
2018-09-19 22:32:08 -0700

    minor improvements in medalists search

--- app_server/public/scripts/searchMedalists.js
+++ app_server/public/scripts/searchMedalists.js
@@ -1,36 +1,23 @@
 function searchme() {
-    var input, filter, table, tr, td, i;
+    var input, filter, table, tr, td, i, col;
     var checkname = document.getElementById("cbred").checked;
     var checkcountry = document.getElementById("cbgreen").checked;
-    //document.getElementById("demo").innerHTML = checkname;
     input = document.getElementById("searchInput");
     filter = input.value.toUpperCase();
-    table = document.getElementById("myTable");
+    table = document.getElementById("searchTable");
     tr = table.getElementsByTagName("tr");
-
-    if (checkname) {
-
-        for (i = 0; i < tr.length; i++) {
-            td = tr[i].getElementsByTagName("td")[0];
-            if (td) {
-                if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-                    tr[i].style.display = "";
-                } else {
-                    tr[i].style.display = "none";
-                }
-            }
-        }
-    }
     if (checkcountry) {
-
-        for (i = 0; i < tr.length; i++) {
-            td = tr[i].getElementsByTagName("td")[1];
-            if (td) {
-                if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-                    tr[i].style.display = "";
-                } else {
-                    tr[i].style.display = "none";
-                }
+        col = 1;
+    } else {
+        col = 0;
+    }
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[col];
+        if (td) {
+            if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
             }
         }
     }
