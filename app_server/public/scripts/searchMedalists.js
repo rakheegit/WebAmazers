81f94a8a22acba6ef6ee3023575cca357b26caec
Ashish Virani <ashishvirani10@gmail.com>
2018-10-04 15:23:26 -0700

    Slider jQuery

--- app_server/public/scripts/searchMedalists.js
+++ app_server/public/scripts/searchMedalists.js
@@ -29,9 +29,61 @@ function displayChange(type) {
 
 function hideShow() {
     var x = document.getElementById("hideImage");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
+    if (x.style.display === "block") {
         x.style.display = "none";
+    } else {
+        x.style.display = "block";
     }
 }
+
+$( function() {
+    $("#slider").slider({
+        value: 2000,
+        range: true,
+        min: 1980,
+        max: 2020,
+        step: 1,
+        change: function(event, ui) {
+            minimum=ui.values[0];
+            maximum=ui.values[1];
+            table = document.getElementById("searchTable");
+            tr = table.getElementsByTagName("tr");
+            for (i = 0; i < tr.length; i++) {
+                td = tr[i].getElementsByTagName("td")[3];
+                console.log(td);
+                if (td) {
+                    const year = td.innerHTML;
+                    if (year<minimum || year>maximum) {
+                        tr[i].style.display = "none";
+                    }
+                    else{
+                        tr[i].style.display = "";
+                    }
+                }
+            }
+        }
+    })
+    .each(function() {
+    
+        // Add labels to slider whose values 
+        // are specified by min, max
+    
+        // Get the options for this slider (specified above)
+        var opt = $(this).data().uiSlider.options;
+    
+        // Get the number of possible values
+        var vals = opt.max - opt.min;
+    
+        // Position the labels
+        for (var i = 0; i <= vals; i+=6) {
+    
+            // Create a new element and position it with percentages
+            var el = $('<label>' + (i + opt.min) + '</label>').css('left', (i/vals*100) + '%');
+    
+            // Add the element inside #slider
+            $("#slider").append(el);
+    
+        }
+    
+    });
+  } );
