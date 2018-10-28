243d5651de7407809cb9047745cb021cbf7b7064
Anudeepa Dinesh <deepashenoy4@gmail.com>
2018-10-28 13:07:20 -0700

    mongo setup

--- webamazers/app.js
+++ webamazers/app.js
@@ -8,14 +8,23 @@ var app = express();
 //View engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'jade');
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); 
 app.use(express.static(path.join(__dirname, '/app_server/public')));
-
 app.use(session( {secret: "String for encrypting cookies." } ));
-
 app.use('/', index);
+
+//mongo connection
+mongoose.connect('mongodb://54.218.249.201:27017/webamazers', {
+    autoReconnect: true, reconnectTries: Number.MAX_VALUE, keepAlive: true
+})
+mongoose.connection.on('connected', () => {
+    console.log("Connected")
+})
+mongoose.connection.on('error', (err) => {
+    console.log(err)
+})
+
 
 module.exports = app;
 app.listen(3000);
--- webamazers/app_server/controllers/main.js
+++ webamazers/app_server/controllers/main.js
@@ -1,17 +1,11 @@
 var registeredUsers = [{ username: "admin@admin.com", password: "admin" }, { username: "ashish@gmail.com", password: "1234" }, { username: "a@g.co", password: "1" }];
+var schemaWebsite = require('../model/websitesSchema')
+var userSchema =  require('../model/user');
 
-
-module.exports.get_websites = function(req, res) {
-    results = [{
-        '_id':1,
-        'Website':'www.google.com',
-        'privacy':'excellent'
-        },{
-        '_id':2,
-        'Website':'www.facebook.com',
-        'privacy':'average'
-        },
-    ] 
-    res.render('websites', { title: 'Express' , websites: results });
+module.exports.get_websites = function (req, res) {
+    schemaWebsite.find(function (err, webs) {
+        res.render('websites', { title: 'Express', websites: webs });
+    })
 };
+
 
