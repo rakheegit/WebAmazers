f8163f003fbb31336f1b560889a11eefa12a1966
Rakhee Singh <rakheesmailbag@gmail.com>
2018-12-02 17:12:18 -0800

    colors for avg monthly visits edu cat

--- webamazers/app_server/public/scripts/dashboard.js
+++ webamazers/app_server/public/scripts/dashboard.js
@@ -26,8 +26,6 @@ function drawChart_newuser() {
                 var tableRow = [];
                 columns.forEach(function(col) {
 
-
-
                     if (col == "Unique_Users") {
 
                         result[col] = parseInt(result[col] / 100000);
@@ -64,6 +62,64 @@ function drawChart_newuser() {
             };
 
             var chart = new google.visualization.ColumnChart(document.getElementById('newuser'));
+            chart.draw(chartData, options);
+        }
+    })
+}
+
+function drawChart_avg_monthly_visits() {
+
+    $.ajax({
+        url: "/dashboard_avg_monthly_visits",
+        type: 'GET',
+        success: function(resData) {
+            console.log(resData.webs);
+            var results = resData.webs;
+            var i = 0;
+            var columns = Object.keys(results[0]);
+            var colors = ["#f4a142", "#f4a142", "grey", "grey", "grey", "grey", "grey", "grey", "grey", "grey"];
+            var data = results.map(function(result) {
+                var tableRow = [];
+                columns.forEach(function(col) {
+
+                    if (col == "Avg_Month_Visits") {
+                        //  result[col] = parseInt(result[col] * 100);
+                        result[col] = parseFloat(result[col]);
+                        tableRow.splice(1, 0, result[col]);
+                    } else if (col == "Domain") {
+                        tableRow.splice(0, 0, result[col]);
+                    }
+                });
+                tableRow.splice(2, 0, colors[i]);
+                i += 1;
+                return tableRow;
+            });
+            var tableRow = [];
+            columns.forEach(function(col) {
+                tableRow.push(col);
+            })
+            tableRow.push({ role: 'style' });
+            data.splice(0, 0, tableRow);
+
+
+            var chartData = google.visualization.arrayToDataTable(data);
+
+            var options = {
+                title: 'Averaga Monthly Visits',
+                //colors: ['#BC70A4'],
+                width: 550,
+                height: 350,
+                legend: { position: "none" },
+                titleTextStyle: {
+                    fontSize: 14, // 12, 18 whatever you want (don't specify px)
+                    bold: true, // true or false
+                },
+                vAxis: { title: "Monthly Visits" }
+                //  vAxis: { format: 'percent' }
+
+            };
+
+            var chart = new google.visualization.ColumnChart(document.getElementById('avg_monthly_visits'));
             chart.draw(chartData, options);
         }
     })
@@ -116,58 +172,6 @@ function drawChart_bouncerate_movies() {
             };
 
             var chart = new google.visualization.ColumnChart(document.getElementById('bounceRate'));
-            chart.draw(chartData, options);
-        }
-    })
-}
-
-function drawChart_avg_monthly_visits() {
-
-    $.ajax({
-        url: "/dashboard_avg_monthly_visits",
-        type: 'GET',
-        success: function(resData) {
-            console.log(resData.webs);
-            var results = resData.webs;
-            var columns = Object.keys(results[0]);
-
-            var data = results.map(function(result) {
-                var tableRow = [];
-                columns.forEach(function(col) {
-
-                    if (col == "Avg_Month_Visits") {
-                        //  result[col] = parseInt(result[col] * 100);
-                        result[col] = parseFloat(result[col]);
-                        tableRow.splice(1, 0, result[col]);
-                    } else if (col == "Domain") {
-                        tableRow.splice(0, 0, result[col]);
-                    }
-                });
-                return tableRow;
-            });
-            var tableRow = [];
-            columns.forEach(function(col) {
-                tableRow.push(col);
-            })
-            data.splice(0, 0, tableRow);
-
-            var chartData = google.visualization.arrayToDataTable(data);
-            var options = {
-                title: 'Averaga Monthly Visits',
-                colors: ['#BC70A4'],
-                width: 550,
-                height: 350,
-                legend: { position: "none" },
-                titleTextStyle: {
-                    fontSize: 14, // 12, 18 whatever you want (don't specify px)
-                    bold: true, // true or false
-                },
-                vAxis: { title: "Monthly Visits" }
-                //  vAxis: { format: 'percent' }
-
-            };
-
-            var chart = new google.visualization.ColumnChart(document.getElementById('avg_monthly_visits'));
             chart.draw(chartData, options);
         }
     })
