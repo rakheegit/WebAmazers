d7778db9a7f00146ebd9cfc7f0e02edbf02197e3
Anudeepa Dinesh <deepashenoy4@gmail.com>
2018-12-04 02:59:19 -0800

    user prompts

--- webamazers/app_server/public/scripts/login.js
+++ webamazers/app_server/public/scripts/login.js
@@ -11,10 +11,18 @@ $("#loginbutton").on("click", function(e) {
     url: "/loginuser",
     data: userdetails,
     success: function(res) {
-      if (res.msg instanceof Object) {
-        window.localStorage.setItem("username", res.msg[0].name)
-        window.localStorage.setItem("pref", res.msg[0].pref)
-        window.location.href = "/home";
+      console.log(res.msg);
+      if (res.msg[0] instanceof Object) {
+        console.log(res.valid)
+        if (res.valid) {
+          window.location.href = "/home";
+        }
+      } else {
+        var error = {
+          title: "Error while logging in",
+          body: res.msg
+        };
+        prompt(error);
       }
     },
     error: function(e) {
@@ -23,3 +31,14 @@ $("#loginbutton").on("click", function(e) {
   });
 });
 
+$(window).on("load", function() {
+  console.log(typeof loggedin);
+  if (loggedin == "false") {
+    var error = {
+      title: "Error while loading page",
+      body: "Please sign in before you can access the page"
+    };
+    prompt(error);
+  } else {
+  }
+});
