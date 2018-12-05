89259b19f00ef9652b4c99f75325657db385ec3b
Anudeepa Dinesh <deepashenoy4@gmail.com>
2018-12-05 15:17:20 -0800

    UI enhance for table

--- webamazers/app_server/public/scripts/bootstrap_popup.js
+++ webamazers/app_server/public/scripts/bootstrap_popup.js
@@ -1,5 +1,4 @@
-window.showBSModal = function self(options) {
-
+window.showBSModal = function self(options, callback) {
     var options = $.extend({
             title : '',
             body : '',
@@ -42,6 +41,51 @@ window.showBSModal = function self(options) {
     } else {
         $('<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>').appendTo(footer);
     }
-
     self.$modal.modal(options);
+    callback()
+    
 }
+
+function dragElement(elmnt) {
+    var elmnt= document.getElementsByClassName("modal fade")[0].children[0].children[0]
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    if (elmnt.children[0]) {
+    //   if present, the header is where you move the DIV from:
+      elmnt.children[0].onmousedown = dragMouseDown;
+    } else {
+    //   otherwise, move the DIV from anywhere inside the DIV: 
+      elmnt.onmousedown = dragMouseDown;
+    }
+  
+    function dragMouseDown(e) {
+      elmnt.children[0].style.cursor='-webkit-grabbing'
+      e = e || window.event;
+      e.preventDefault();
+      // get the mouse cursor position at startup:
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      document.onmouseup = closeDragElement;
+      // call a function whenever the cursor moves:
+      document.onmousemove = elementDrag;
+    }
+  
+    function elementDrag(e) {
+      e = e || window.event;
+      e.preventDefault();
+      // calculate the new cursor position:
+      pos1 = pos3 - e.clientX;
+      pos2 = pos4 - e.clientY;
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      // set the element's new position:
+      elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+      elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+  
+    function closeDragElement() {
+      elmnt.children[0].style.cursor='-webkit-grab'
+      // stop moving when mouse button is released:
+      document.onmouseup = null;
+      document.onmousemove = null;
+    }
+  }
